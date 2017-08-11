b0f8fa2efb5206059444d8c36c042ad2fddaeb18
Papushe <papushe9@gmail.com>
2017-08-11 09:25:02 +0300

    bug fixed

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     properties: {
         MLAB_KEY: 'mongodb://db_usr:db_pass@ds115752.mlab.com:15752/magneto',
+        AWS: 'mongodb://34.212.119.65:27017/test',
         PORT:8898,
         STATUS_OK:200
     }
